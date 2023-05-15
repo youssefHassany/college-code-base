01aac377cef486bbd3c7a6db66a4b7314c30629c
Youssef <y.2003eg@gmail.com>
2023-05-15 15:09:29 +0300

    practical 8 Q2 edited

--- practical 8/Q2/Main.java
+++ practical 8/Q2/Main.java
@@ -1,14 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         int[] nums = { 7, 10, 5, 3, 6, 2, 0, 1, 8, 4 };
-        int max = nums[0];
+
+        int max = nums[0]; // for now
+        int indexOfMax = 0; // for now
 
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] > max) {
                 max = nums[i];
+                indexOfMax = i;
             }
         }
 
         System.out.println("Max = " + max); // 10
+        System.out.println("index of max = " + indexOfMax); // 1
     }
 }
