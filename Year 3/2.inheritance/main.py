e5c9284200ec04f0a72df2d0b41b2f42021a2497
Youssef Hassany <youssefhassany22@gmail.com>
2024-05-13 16:29:06 +0300

    first video recorded

---
+++ Year 3/2.inheritance/main.py
@@ -0,0 +1,10 @@
+# 1) What is inheritance
+# 2) when to use inheritance
+# 3) What is polymorphism
+# 4) when to use polymorphism
+
+# ------------------------------------------------------
+
+# create 4 people (classes) , 1 student, 1 athelete, 1 actress, 1 singer
+# each has first name and last name and a method to print their info "showInfo"
+# the rest of the proprieties are unique
