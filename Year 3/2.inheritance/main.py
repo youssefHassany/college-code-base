c80a94c2586f2ba17b89a3a9e4632745fb40ca62
Youssef Hassany <youssefhassany22@gmail.com>
2024-05-13 16:50:00 +0300

    second video recorded

--- Year 3/2.inheritance/main.py
+++ Year 3/2.inheritance/main.py
@@ -8,3 +8,31 @@
 # create 4 people (classes) , 1 student, 1 athelete, 1 actress, 1 singer
 # each has first name and last name and a method to print their info "showInfo"
 # the rest of the proprieties are unique
+
+class Person:
+    def __init__(self, firstName, lastName):
+        self.firstName = firstName
+        self.lastName = lastName
+    
+    def showInfo(self):
+        print(f'{self.firstName} {self.lastName}')
+
+
+class Student(Person):
+    def __init__(self, firstName, lastName, gpa):
+        super().__init__(firstName, lastName)
+        self.gpa = gpa
+
+    def showInfo(self):
+        print(f'{self.firstName} {self.lastName} his GPA is {self.gpa}')
+
+
+
+class Athelete(Person):
+    def __init__(self, firstName, lastName, benchPressWeight):
+        super().__init__(firstName, lastName)
+        self.benchPressWeight = benchPressWeight
+
+
+a = Athelete("David", "Laid", 100)
+a.showInfo()
